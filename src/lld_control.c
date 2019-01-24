4d3bd54fb0530e98fa4baeb0a2a2daaa62d60afe
Elena <seryhelena@gmail.com>
2019-01-24 20:54:36 +0300

    [lld_control] initial

--- src/lld_control.c
+++ src/lld_control.c
@@ -1,8 +1,77 @@
 #include <tests.h>
 #include <lld_control.h>
 
+#define pwm1Freq        500000
+#define pwm1Period      10000           // 50 Hz
+
+/***  PWM configuration pins    ***/
+/***  PE9 - Steering            ***/
+#define PE9_ACTIVE      PWM_OUTPUT_ACTIVE_HIGH
+#define PE9_DISABLE     PWM_OUTPUT_DISABLED
+#define steerPWMch      0
+/***  PE11 - Braking            ***/
+#define PE11_ACTIVE     PWM_OUTPUT_ACTIVE_HIGH
+#define PE11_DISABLE    PWM_OUTPUT_DISABLED
+#define brakePWMch      1
+/***  PE13, PE14 - not used     ***/
+#define PE13_ACTIVE     PWM_OUTPUT_ACTIVE_HIGH
+#define PE13_DISABLE    PWM_OUTPUT_DISABLED
+#define PE14_ACTIVE     PWM_OUTPUT_ACTIVE_HIGH
+#define PE14_DISABLE    PWM_OUTPUT_DISABLED
+
+static  PWMDriver       *pwmDriver      = &PWMD1;
+
+#define pwm1LineCh0     PAL_LINE( GPIOE, 9 )
+#define pwm1LineCh1     PAL_LINE( GPIOE, 11 )
+
+/*** Configuration structures ***/
+
+PWMConfig pwm1conf = {
+    .frequency = pwm1Freq,
+    .period    = pwm1Period, /* 1/1000 s = 10 ms => 100 Hz
+                             * PWM period = period/frequency [s] */
+    .callback  = NULL,
+    .channels  = {
+                  {.mode = PE9_ACTIVE,      .callback = NULL},
+                  {.mode = PE11_ACTIVE,     .callback = NULL},
+                  {.mode = PE13_DISABLE,    .callback = NULL},
+                  {.mode = PE14_DISABLE,    .callback = NULL}
+                  },
+    .cr2        = 0,
+    .dier       = 0
+};
+
+static bool         isInitialized       = false;
+
+
+/**
+ * @brief   Initialize periphery connected to driver control
+ * @note    Stable for repeated calls
+ */
 void lldControlInit( void )
 {
+    if ( isInitialized )
+        return;
 
+    /*** PWM pins configuration ***/
+    palSetLineMode( pwm1LineCh0,  PAL_MODE_ALTERNATE(1) );
+    palSetLineMode( pwm1LineCh1,  PAL_MODE_ALTERNATE(1) );
+
+    pwmStart( pwmDriver, &pwm1conf );
+
+    /* Set initialization flag */
+
+    isInitialized = true;
 
 }
+
+/*
+ * @brief   Set power for steering motor (via ESC)
+ * @param   inputPrc   Motor power value [-100 100]
+ * @note    control signal - [1 2] ms => [4000 8000]
+ */
+void lldControlSetSteerPower( controlValue_t inputPrc )
+{
+
+}
+
