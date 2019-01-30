b38e7439c4204bcb2400cbf4ceb3bb6880362978
Oleg <kopatychev@mail.ru>
2019-01-30 15:49:55 +0300

    [common] #31 fixed

--- include/common.h
+++ include/common.h
@@ -14,31 +14,5 @@
 #define     MAIN_PROGRAM_ROUTINE                        PROGRAM_ROUTINE_TEST_ENCODER
 
 
-#define ADC_RES_CONF                ADC_CR1_12B_RESOLUTION
-
-/*Steer sensor position input */
-#define ADC_SEQ1_NUM                ADC_CHANNEL_IN10
-#define ADC_SEQ1_LINE               PAL_LINE( GPIOC, 0 )
-#define ADC_SEQ1_CH                 0
-
-/**
- * @brief       Preparing to start ADC operations
-*/
-void InitAdc ( void );
-
-/**
- * @brief       Get value of ADC channel
- * @return      Value of ADC sampling
-*/
-adcsample_t FindADC1Val ( void );
-
-
-
-/* Additional ADC constants */
-#define ADC_CR1_12B_RESOLUTION      (0)
-#define ADC_CR1_10B_RESOLUTION      (ADC_CR1_RES_0)
-#define ADC_CR1_8B_RESOLUTION       (ADC_CR1_RES_1)
-#define ADC_CR1_6B_RESOLUTION       (ADC_CR1_RES_0 | ADC_CR1_RES_1)
-
 
 #endif /* INCLUDE_COMMON_H_ */
