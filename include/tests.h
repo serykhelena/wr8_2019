a880f3cdb4dc037b3eb03e137c0356354fd99b97
Anastasia Rudikova <ryd.as@mail.ru>
2019-02-06 18:28:25 +0300

    [add]: include test

--- include/tests.h
+++ include/tests.h
@@ -13,17 +13,48 @@
  * @note    SD7 is used for testing (PE7, PE8)
  */
 
+void testEncoder (void);
+
+/****************************/
+/*** Driver Control tests ***/
+/****************************/
+
+/*
+ * @brief   In test: control and get values of speed and angle of rotation
+ * @note    The routine has internal infinite loop
+ * @note    SD7 is used for testing (PE7, PE8)
+ */
+
+void testDriverControlRoutine (void);
+
+/****************************/
+/*** Remote Control tests ***/
+/****************************/
+
+/*
+ * @brief   In test:
+ * @note    The routine has internal infinite loop
+ * @note    SD7 is used for testing (PE7, PE8)
+ */
+
 void TestRMControl (void);
 
+/*************************/
+/*** Tests application ***/
+/*************************/
 
+/**
+ * @brief   Routines of tests
+ */
 static inline void testsRoutines( void )
 {
-#if (MAIN_PROGRAM_ROUTINE == PROGRAM_ROUTINE_TEST_RM_CONTROL)
-
-  TestRMControl();
-
-
-#endif
+  #if (MAIN_PROGRAM_ROUTINE == PROGRAM_ROUTINE_TEST_ENCODER)
+    testEncoder();
+  #elif (MAIN_PROGRAM_ROUTINE == PROGRAM_ROUTINE_TEST_LL_DRIVER)
+    testDriverControlRoutine();
+  #elif (MAIN_PROGRAM_ROUTINE == PROGRAM_ROUTINE_TEST_RM_CONTROL)
+    TestRMControl();
+  #endif
 }
 
 #endif /* INCLUDE_TESTS_H_ */
