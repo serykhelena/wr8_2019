c71f7a63ca6c4e64f726a744d5e85067cee8ec9e
Anastasia Rudikova <ryd.as@mail.ru>
2019-02-11 17:01:20 +0300

    [issues]: bugs fixed

--- include/common.h
+++ include/common.h
@@ -17,7 +17,6 @@
 #define CLIP_VALUE(x, min, max) ((x) < (min) ? (min) :      \
                                  (x) > (max) ? (max) : (x))
 
-<<<<<<< HEAD
 /**************************/
 /*** CONFIGURATION ZONE ***/
 /**************************/
@@ -60,8 +59,7 @@
 /******************************/
 /*** CONFIGURATION ZONE END ***/
 /******************************/
-=======
->>>>>>> ed87bdeacda3aa89c6954fbf65c6288514ad4730
+
 
 
 #endif /* INCLUDE_COMMON_H_ */
--- src/lld_control.c
+++ src/lld_control.c
@@ -1,50 +1,6 @@
 #include <tests.h>
 #include <lld_control.h>
-<<<<<<< HEAD
 #include <math.h>
-=======
-#include <common.h>
-
-/**************************/
-/*** CONFIGURATION ZONE ***/
-/**************************/
-
-#define SPEEDmin           -100
-#define SPEEDmax            100
-
-#define SPEED_DUTY_K_max    4
-#define SPEED_DUTY_B_max    1520
-
-#define SPEED_DUTY_K_min    2.6
-#define SPEED_DUTY_B_min    1420
-
-#define STEERmin           -100
-#define STEERmax            100
-
-#define STEER_DUTY_K        4
-#define STEER_DUTY_B        1560
-
-//*        |  Clockwise  |  Center  | Counterclockwise
-// * -------------------------------------------------
-// * t, ms  |     1.16    |   1.56   |      1.92
-// * -------------------------------------------------
-// * width  |     1160    |   1560   |      1920
-// * -------------------------------------------------
-// *        |on the right |  Center  |   On the left
-// *
-// * PD_15 => Driving wheels (Channel 2)
-// *
-// *        | Backward  |     Center      | Forward
-// *-------------------------------------------
-// * t, ms  |    1.24   |    1.4 - 1.5    |   1.6
-// * ------------------------------------------
-// * width  |    1240   |   1400 - 1500   |   1600
-// *
-
-/******************************/
-/*** CONFIGURATION ZONE END ***/
-/******************************/
->>>>>>> ed87bdeacda3aa89c6954fbf65c6288514ad4730
 
 /*** Hardware configuration     ***/
 /***  PWM configuration pins    ***/
