e5e24f81f18d99ae1042024d124a7980a0049a14
Oleg <kopatychev@mail.ru>
2019-02-26 01:47:24 +0300

    [charge level]: add ADC3 set

--- include/common.h
+++ include/common.h
@@ -60,6 +60,55 @@
 /*** CONFIGURATION ZONE END ***/
 /******************************/
 
+#define ADC_RES_CONF                ADC_CR1_12B_RESOLUTION
 
+/*Steer sensor position input */
+#define ADC1_SEQ1_NUM                ADC_CHANNEL_IN10
+#define ADC1_SEQ1_LINE               PAL_LINE( GPIOC, 0 )
+#define ADC1_SEQ1_CH                 0
+
+/*Charge level 1 */
+#define ADC3_SEQ1_NUM                ADC_CHANNEL_IN9
+#define ADC3_SEQ1_LINE               PAL_LINE( GPIOF, 3 )
+#define ADC3_SEQ1_CH                 0
+
+/*Charge level 2 */
+#define ADC3_SEQ2_NUM                ADC_CHANNEL_IN15
+#define ADC3_SEQ2_LINE               PAL_LINE( GPIOF, 5 )
+#define ADC3_SEQ2_CH                 1
+
+/**
+ * @brief       Preparing to start ADC1 operations
+*/
+void InitAdc1 ( void );
+
+/**
+ * @brief       Preparing to start ADC2 operations
+*/
+void InitAdc3 ( void );
+
+/**
+ * @brief       Get value of ADC1 channel 0
+ * @return      Value of ADC sampling
+*/
+adcsample_t GetAdcValSteer (void);
+
+/**
+ * @brief       Get value of ADC3 channel 0
+ * @return      Value of ADC sampling
+*/
+adcsample_t GetAdcVal_STMPower (void);
+
+/**
+ * @brief       Get value of ADC3 channel 1
+ * @return      Value of ADC sampling
+*/
+adcsample_t GetAdcVal_Battery (void);
+
+/* Additional ADC constants */
+#define ADC_CR1_12B_RESOLUTION      (0)
+#define ADC_CR1_10B_RESOLUTION      (ADC_CR1_RES_0)
+#define ADC_CR1_8B_RESOLUTION       (ADC_CR1_RES_1)
+#define ADC_CR1_6B_RESOLUTION       (ADC_CR1_RES_0 | ADC_CR1_RES_1)
 
 #endif /* INCLUDE_COMMON_H_ */
