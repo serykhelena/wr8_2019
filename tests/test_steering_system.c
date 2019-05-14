d8a3e8dbe3a0fae3bbf2e596b931ba1b312fafd4
Anastasia Rudikova <ryd.as@mail.ru>
2019-05-14 14:54:31 +0300

    [remotecontrol]: delete branch

--- tests/test_steering_system.c
+++ tests/test_steering_system.c
@@ -5,5 +5,33 @@
 
 void testSteeringSystem(void)
 {
+  float test_steer_cs = 0;
+  controlValue_t delta_steer_cs = 1;
 
+  ICUInit2();
+  lldControlInit();
+  debug_stream_init();
+
+  dbgprintf("TEST STEERING SYSTEM\r");
+
+  while(true)
+  {
+    char rcv_data_cs = sdGet (&SD7);
+    switch (rcv_data_cs)
+    {
+    case 'a':
+      test_steer_cs -= delta_steer_cs;
+      break;
+
+    case 's':
+      test_steer_cs += delta_steer_cs;
+
+    }
+
+
+
+    lldSteeringControlGetAngle();
+
+    dbgprintf("\t Steer(%d)\n\r ", steer_cs);
+  }
 }
